706ff103b8b1d832d789e47ca8d8a69637c2fd53
joliveros <oliverosjose@gmail.com>
2017-03-27 20:34:01 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/com/mycompany/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.mycompany.com.mycompany.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by joliveros on 27/03/2017.
+ */
+@Controller
+public class WelcomeController {
+    @Value("${welcome.message:test}")
+    private String message = "Hello World!";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+}
